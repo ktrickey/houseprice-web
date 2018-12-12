09171ba37cc098c57664ddbf5e6d19e993c7fd64
Kevin Trickey <registrations@kevintrickey.me.uk>
2018-12-12 22:06:13 +0000

    Indents

--- src/HousePrices.Web/Controllers/HomeController.cs
+++ src/HousePrices.Web/Controllers/HomeController.cs
@@ -17,120 +17,118 @@ namespace HousePrices.Web.Controllers
         public long TotalRows { get; set; }
         public IEnumerable<T> Results { get; set; }
     }
-	public class Results
-	{
-		public double Price { get; set; }
-		public DateTime TransferDate { get; set; }
-		public string Postcode { get; set; }
-		public string PropertyType { get; set; }
-		public string IsNew { get; set; }
-		public string Duration { get; set; }
-		public string PAON { get; set; }
-		public string SAON { get; set; }
-		public string Street { get; set; }
-		public string Locality { get; set; }
-		public string City { get; set; }
-		public string District { get; set; }
-		public string County { get; set; }
-		public string CategoryType { get; set; }
-		public string Status { get; set; }
-	}
-	public class HomeController : Controller
-	{
-		private string _apiRoot;
-		public HomeController(IHostingEnvironment env)
-		{
-			var builder = new ConfigurationBuilder()
-				.SetBasePath(Directory.GetCurrentDirectory())
-				
-				.AddJsonFile("appsettings.json")
-				.AddEnvironmentVariables();
+    public class Results
+    {
+        public double Price { get; set; }
+        public DateTime TransferDate { get; set; }
+        public string Postcode { get; set; }
+        public string PropertyType { get; set; }
+        public string IsNew { get; set; }
+        public string Duration { get; set; }
+        public string PAON { get; set; }
+        public string SAON { get; set; }
+        public string Street { get; set; }
+        public string Locality { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string County { get; set; }
+        public string CategoryType { get; set; }
+        public string Status { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        private string _apiRoot;
+        public HomeController(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
 
-			var configuration = builder.Build();
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
 
-
-			_apiRoot = configuration["ApiRoot"];
-			Log.Information($"ApiRoot = {_apiRoot}");
-
-			Log.Information($"Web root path:{env.WebRootPath}, Content root path:{env.ContentRootPath}");
-			this.ViewBag.mappedPath = env.WebRootPath;
-		}
-		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult About()
-		{
-			ViewData["Message"] = "Your application description page.";
-
-			return View();
-		}
-
-		public IActionResult Contact()
-		{
-			ViewData["Message"] = "Your contact page.";
-
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		public class SearchStructure
-		{
-			public string Postcode { get; set; }
-			public double Radius { get; set; }
-		}
+            var configuration = builder.Build();
 
 
+            _apiRoot = configuration["ApiRoot"];
+            Log.Information($"ApiRoot = {_apiRoot}");
 
-		[HttpGet]
-		public IActionResult SearchResults(SearchStructure search)
-		{
-			var url = $"{_apiRoot}/api/transaction/{search.Postcode}/{search.Radius}";
-		
-			Log.Information($"accessing {url}");
-			try
-			{
+            Log.Information($"Web root path:{env.WebRootPath}, Content root path:{env.ContentRootPath}");
+            this.ViewBag.mappedPath = env.WebRootPath;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-				WebRequest request = WebRequest.Create(url);
-				// If required by the server, set the credentials.
-				request.Credentials = CredentialCache.DefaultCredentials;
-				//"STUFF".Humanize(LetterCasing.Sentence);
-				// Get the response.
-				HttpWebResponse response = (HttpWebResponse) request.GetResponse();
-				// Display the status.
-				Log.Information(response.StatusDescription);
-				// Get the stream containing content returned by the server.
-				Stream dataStream = response.GetResponseStream();
-				// Open the stream using a StreamReader for easy access.
-				StreamReader reader = new StreamReader(dataStream);
-				// Read the content.
-				string responseFromServer = reader.ReadToEnd();
-				// Display the content.
-				Console.WriteLine(responseFromServer);
-				// Cleanup the streams and the response.
-				reader.Close();
-				dataStream.Close();
-				response.Close();
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
 
-				var stuff = Newtonsoft.Json.JsonConvert.DeserializeObject<PagedResult<Results>>(responseFromServer);
-				return View(stuff);
-			}
-			catch (Exception ex)
-			{
-				Log.Information($"Error accessing {url}:{ex.Message}");
-				throw;
-			}
-		}
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public class SearchStructure
+        {
+            public string Postcode { get; set; }
+            public double Radius { get; set; }
+        }
+
+        [HttpGet]
+        public IActionResult SearchResults(SearchStructure search)
+        {
+            var url = $"{_apiRoot}/api/transaction/{search.Postcode}/{search.Radius}";
+
+            Log.Information($"accessing {url}");
+            try
+            {
+
+                WebRequest request = WebRequest.Create(url);
+                // If required by the server, set the credentials.
+                request.Credentials = CredentialCache.DefaultCredentials;
+                //"STUFF".Humanize(LetterCasing.Sentence);
+                // Get the response.
+                HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+                // Display the status.
+                Log.Information(response.StatusDescription);
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                var stuff = Newtonsoft.Json.JsonConvert.DeserializeObject<PagedResult<Results>>(responseFromServer);
+                return View(stuff);
+            }
+            catch (Exception ex)
+            {
+                Log.Information($"Error accessing {url}:{ex.Message}");
+                throw;
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
--- src/HousePrices.Web/Program.cs
+++ src/HousePrices.Web/Program.cs
@@ -6,37 +6,37 @@ using Serilog.Events;
 
 namespace HousePrices.Web
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			Log.Logger = new LoggerConfiguration()
-				.MinimumLevel.Debug()
-				.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-				.Enrich.FromLogContext()
-				.WriteTo.Console()
-				.CreateLogger();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-			try
-			{
-				Log.Information("Starting web host");
-				CreateWebHostBuilder(args).Build().Run();
-			}
-			catch (Exception ex)
-			{
-				Log.Fatal(ex, "Host terminated unexpectedly");
-			}
-			finally
-			{
-				Log.CloseAndFlush();
-			}
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
 
-			
-		}
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
-				.UseStartup<Startup>()
-				.UseSerilog();
-	}
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
 }
